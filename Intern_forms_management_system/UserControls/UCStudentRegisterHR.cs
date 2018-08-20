7ab7e4d9b52a91f6600d9df74b9c385f5240daf5
niharai <nihara1994@gmail.com>
2018-08-21 02:00:36 +0530

    Further additions and validations to HR Interface

--- Intern_forms_management_system/UserControls/UCStudentRegisterHR.cs
+++ Intern_forms_management_system/UserControls/UCStudentRegisterHR.cs
@@ -26,5 +26,105 @@ namespace Intern_forms_management_system.UserControls
         {
 
         }
+
+        private void bunifuMaterialTextbox5_OnValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuFlatButton1_Click(object sender, EventArgs e)
+        {
+            //Button for inserting an employee to the Company Database
+            if (NameTextBox.Text == "" || NameTextBox.Text == "Enter name here....." || UniversityTextBox.Text == "" || AddressTextBox.Text == "" || EmailTextBox.Text == "" || TelephoneTextBox.Text == "" || DepartmentTextBox.Text == "")
+            {
+                MessageBox.Show("Please Fill all the fields.....");
+
+            }
+            else {
+
+            }
+        }
+
+        private void bunifuDatepicker1_onValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuMaterialTextbox1_OnValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NameTextBox_Enter(object sender, EventArgs e)
+        {
+            NameTextBox.Text = "";
+        }
+
+        private void NameTextBox_Leave(object sender, EventArgs e)
+        {
+            //NameTextBox.Text = "Enter Name here ...";
+        }
+
+        private void UniversityTextBox_Enter(object sender, EventArgs e)
+        {
+            UniversityTextBox.Text = "";
+        }
+
+        private void UniversityTextBox_Leave(object sender, EventArgs e)
+        {
+            //UniversityTextBox.Text = "Enter the University here ...";
+        }
+
+        private void AddressTextBox_OnValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddressTextBox_Enter(object sender, EventArgs e)
+        {
+            AddressTextBox.Text = "";
+        }
+
+        private void AddressTextBox_Leave(object sender, EventArgs e)
+        {
+            //AddressTextBox.Text = "Enter Address here ...";
+        }
+
+        private void EmailTextBox_Enter(object sender, EventArgs e)
+        {
+            EmailTextBox.Text = "";
+        }
+
+        private void EmailTextBox_Leave(object sender, EventArgs e)
+        {
+            //EmailTextBox.Text = "Enter Email here ...";
+        }
+
+        private void TelephoneTextBox_OnValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TelephoneTextBox_Enter(object sender, EventArgs e)
+        {
+            TelephoneTextBox.Text = "";
+        }
+
+        private void DepartmentTextBox_Enter(object sender, EventArgs e)
+        {
+            DepartmentTextBox.Text = "";
+        }
+
+        private void ClearEmployee_Click(object sender, EventArgs e)
+        {
+            //Clear the text in text boxes
+           
+            NameTextBox.Text = "";
+            AddressTextBox.Text = "";
+            EmailTextBox.Text = "";
+            DepartmentTextBox.Text = "";
+            TelephoneTextBox.Text = "";
+            UniversityTextBox.Text = "";
+        }
     }
 }
