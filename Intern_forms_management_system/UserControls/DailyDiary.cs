d3bbb662dce208a8e381c6c126193b684d3e240c
unknown <mwindula16@gmail.com>
2018-08-22 01:59:28 +0530

    diary entry UI initial commit

---
+++ Intern_forms_management_system/UserControls/DailyDiary.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Intern_forms_management_system.UserControls
+{
+    public partial class DailyDiary : UserControl
+    {
+        public DailyDiary()
+        {
+            InitializeComponent();
+        }
+
+        private void bunifuCustomLabel2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuCards1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void bunifuMaterialTextbox5_OnValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuFlatButton1_Click(object sender, EventArgs e)
+        {
+            //Button for inserting an employee to the Company Database
+            
+        }
+
+        private void bunifuDatepicker1_onValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuMaterialTextbox1_OnValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
