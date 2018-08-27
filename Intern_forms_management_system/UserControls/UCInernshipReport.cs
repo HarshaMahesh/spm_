f2061429219e4e6d6ca570554c392b738bfae80f
niharai <nihara1994@gmail.com>
2018-08-27 08:31:47 +0530

    Internship Report UI created and bugs in 2nd sprint listed

---
+++ Intern_forms_management_system/UserControls/UCInernshipReport.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Intern_forms_management_system.UserControls
+{
+    public partial class UCInernshipReport : UserControl
+    {
+        public UCInernshipReport()
+        {
+            InitializeComponent();
+        }
+    }
+}
