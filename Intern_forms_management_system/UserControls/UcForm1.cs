2465a00b46829f045a32aa7eff78ad49774618dd
HarshaLG <harsha.mahesh@my.sliit.lk>
2018-09-04 20:56:53 +0530

    Create employee UI for FormI-1 and Dboperation

--- Intern_forms_management_system/UserControls/UcForm1.cs
+++ Intern_forms_management_system/UserControls/UcForm1.cs
@@ -8,15 +8,27 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using MetroFramework;
+using MySql.Data.MySqlClient;
 
 namespace Intern_forms_management_system.UserControls
 {
     public partial class UcForm1 : UserControl
     {
         DbOparation dbo=new DbOparation();
+        DbConnect db = new DbConnect();
         public UcForm1()
         {
             InitializeComponent();
+        }
+        public void studentEmpty() {
+            stdnametbox.Text ="";
+            addresstbox.Text = "";
+            mphonetbox.Text = "";
+            emailtbox.Text = "";
+            hphonetbox.Text = "";
+            cgpatbox.Text = "";
+            yeartbox.Text = "";
+            semtbox.Text = "";
         }
 
         private void bunifuCustomLabel2_Click(object sender, EventArgs e)
@@ -73,11 +85,64 @@ namespace Intern_forms_management_system.UserControls
         {
             dbo.addEmployee(Enametbox.Text,Eaddresstbox.Text,snametbox.Text,stitletbox.Text,sphonetbox.Text,semailtbox.Text,esnametbox.Text);
             MetroMessageBox.Show(this, "Successfully Inserted","", MessageBoxButtons.OK);
+
+
         }
 
         private void bunifuFlatButton1_Click(object sender, EventArgs e)
         {
+            double cgpa = Convert.ToDouble(cgpatbox.Text);
+            dbo.addStudentForm1(Sidtxtbox.Text,cgpa, Convert.ToInt32(hphonetbox.Text),semtbox.Text,yeartbox.Text);
+            MetroMessageBox.Show(this, "Successfully Inserted", "", MessageBoxButtons.OK);
+            studentEmpty();
 
+        }
+
+        private void bunifuCustomLabel25_Click(object sender, EventArgs e)
+        {
+            // dbo.searchSudent(Sidtxtbox.Text,snametbox.Text, addresstbox.Text, mphonetbox.Text,semailtbox.Text);
+            //MetroMessageBox.Show(this, "Successfully Inserted", "", MessageBoxButtons.OK);
+            
+        }
+
+        private void bunifuFlatButton9_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                db.Connection();
+                MySqlCommand cmd2;
+                MySqlDataReader mdr;
+
+                String quary = "SELECT * FROM `intern_student` where studentId='" + Sidtxtbox.Text + "'";
+                cmd2 = new MySqlCommand(quary, db.con);
+                db.con.Open();
+                mdr = cmd2.ExecuteReader();
+
+                if (mdr.Read())
+                {
+                    stdnametbox.Text = mdr.GetString("full_name");
+                    addresstbox.Text = mdr.GetString("address");
+                    mphonetbox.Text = mdr.GetString("telephone");
+                    emailtbox.Text = mdr.GetString("email");
+                    hphonetbox.Text = mdr.GetString("homePhone");
+                    cgpatbox.Text = mdr.GetString("cgpa");
+                    yeartbox.Text = mdr.GetString("year");
+                    semtbox.Text = mdr.GetString("semester");
+                }
+                else
+                {
+                    MetroMessageBox.Show(this, "Searched results are empty", "", MessageBoxButtons.OK);
+
+                }
+            }
+            catch (Exception e1) {
+                Console.Write(e1);
+            }
+        }
+
+        private void bunifuFlatButton3_Click(object sender, EventArgs e)
+        {
+            studentEmpty();
         }
     }
 }
