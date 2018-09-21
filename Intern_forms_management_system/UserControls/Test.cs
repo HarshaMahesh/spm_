061ab67e71072bc65ecd8365515c2da915ff410b
Windula <mwindula16@gmail.com>
2018-09-21 13:57:55 +0530

    test commit

---
+++ Intern_forms_management_system/UserControls/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Intern_forms_management_system.UserControls
+{
+    public partial class Test : Form
+    {
+        public Test()
+        {
+            InitializeComponent();
+        }
+    }
+}
